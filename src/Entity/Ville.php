700735dbe16aefcda65a0faa3903b021b09e72a0
dyrits <dyrits@outlook.fr>
2020-10-19 12:01:10 +0300

    New Entity: Etat | Minor update

--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -30,7 +30,7 @@ class Ville
     private $codePostal;
 
     /**
-     * @ORM\OneToMany(targetEntity=Lieu::class, mappedBy="ville", orphanRemoval=true)
+     * @ORM\OneToMany(targetEntity=Lieu::class, mappedBy="ville")
      */
     private $lieus;
 
